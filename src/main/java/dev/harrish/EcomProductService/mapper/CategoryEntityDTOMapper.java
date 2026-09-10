ea8b9663df9c9a200e4cba0d4153e38fe505d04f
Harrish M <harrishmuthu66@gmail.com>
2026-09-10 19:39:08 +0530

    Added category mapper

---
+++ src/main/java/dev/harrish/EcomProductService/mapper/CategoryEntityDTOMapper.java
@@ -0,0 +1,12 @@
+package dev.harrish.EcomProductService.mapper;
+
+import dev.harrish.EcomProductService.dto.CategoryResponseDTO;
+import dev.harrish.EcomProductService.entity.Category;
+
+public class CategoryEntityDTOMapper
+{
+    public static CategoryResponseDTO convertCategoryToCategoryResponseDTO(Category category)
+    {
+        //call Product entity to ProductResponseDTO mapper
+    }
+}
